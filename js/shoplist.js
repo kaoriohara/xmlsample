1b320853308c3cb2d44024bc03a081ced3003544
kaoriohara <kaorin10815@gmail.com>
2019-06-11 23:11:03 +0900

    no message

--- js/shoplist.js
+++ js/shoplist.js
@@ -1,24 +1,37 @@
-
 $(function () {
   $.ajax({
     url: 'shoplist.xml',
     dataType: 'xml',
+    error: function () {
+      alert("ロード失敗");
+    },
     success: function (xml) {
-      // NEWSページ
-
-      var insertContents = '';
-      $('item', xml).each(function () {
-        var thisItems = $(this);
-        insertContents += '<li>';
-        insertContents += '<p class="shop-name">';
-        insertContents += thisItems.children('name').text();
-        insertContents += '</p class="shop-name">';
-        insertContents += '<p class="shop-address">';
-        insertContents += thisItems.children('address').text();
-        insertContents += '</p class="shop-address">';
-        insertContents += '</li>';
+      $(xml).find("shop").each(function () {
+        $(".linkbox-shoplist").append('<li><p class="shop-name">' + $(this).find('name').text() '</p>' + '<p class="shop-address">' + $(this).find('address').text() '</p></li>');
       });
-      $('.linkbox-shoplist').append(insertContents);
     }
   });
 });
+// $(function () {
+//   $.ajax({
+//     url: 'shoplist.xml',
+//     dataType: 'xml',
+//     success: function (xml) {
+//       // NEWSページ
+
+//       var insertContents = '';
+//       $('item', xml).each(function () {
+//         var thisItems = $(this);
+//         insertContents += '<li>';
+//         insertContents += '<p class="shop-name">';
+//         insertContents += thisItems.children('name').text();
+//         insertContents += '</p class="shop-name">';
+//         insertContents += '<p class="shop-address">';
+//         insertContents += thisItems.children('address').text();
+//         insertContents += '</p class="shop-address">';
+//         insertContents += '</li>';
+//       });
+//       $('.linkbox-shoplist').append(insertContents);
+//     }
+//   });
+// });
