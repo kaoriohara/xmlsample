eee9aaa9e7e2a6e4090c4ff0a4cad9f509adcf9f
kaoriohara <kaorin10815@gmail.com>
2019-06-12 16:56:45 +0900

    no message

--- js/shoplist.js
+++ js/shoplist.js
@@ -17,27 +17,27 @@ $(function () {
   $.ajax({
     url: 'shoplist.xml',
     dataType: 'xml',
-    error: function (XMLHttpRequest, textStatus, errorThrown) {
-      alert('error!!!');
-      console.log("XMLHttpRequest : " + XMLHttpRequest.status);
-      console.log("textStatus     : " + textStatus);
-      console.log("errorThrown    : " + errorThrown.message);
-    },
+    // error: function (XMLHttpRequest, textStatus, errorThrown) {
+    //   alert('error!!!');
+    //   console.log("XMLHttpRequest : " + XMLHttpRequest.status);
+    //   console.log("textStatus     : " + textStatus);
+    //   console.log("errorThrown    : " + errorThrown.message);
+    // },
     success: function (xml) {
 
-      var insertContents = '';
+      var insertContent = '';
       $('item', xml).each(function () {
         var thisItems = $(this);
-        insertContents += '<li>';
-        insertContents += '<p class="shop-name">';
-        insertContents += thisItems.children('name').text();
-        insertContents += '</p class="shop-name">';
-        insertContents += '<p class="shop-address">';
-        insertContents += thisItems.children('address').text();
-        insertContents += '</p class="shop-address">';
-        insertContents += '</li>';
+        insertContent += '<li>';
+        insertContent += '<p class="shop-name">';
+        insertContent += thisItems.children('name').text();
+        insertContent += '</p class="shop-name">';
+        insertContent += '<p class="shop-address">';
+        insertContent += thisItems.children('address').text();
+        insertContent += '</p class="shop-address">';
+        insertContent += '</li>';
       });
-      $('.linkbox-shoplist').append(insertContents);
+      $('.linkbox-shoplist').append(insertContent);
     }
   });
 });
