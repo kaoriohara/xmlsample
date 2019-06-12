ec84dbfe3e33b4d8f47623ecf706e4b2456068f5
kaoriohara <kaorin10815@gmail.com>
2019-06-12 16:52:28 +0900

    no message

--- js/news.js
+++ js/news.js
@@ -24,3 +24,32 @@ $(function () {
     }
   });
 });
+
+$(function () {
+  $.ajax({
+    url: 'shoplist.xml',
+    dataType: 'xml',
+    error: function (XMLHttpRequest, textStatus, errorThrown) {
+      alert('error!!!');
+      console.log("XMLHttpRequest : " + XMLHttpRequest.status);
+      console.log("textStatus     : " + textStatus);
+      console.log("errorThrown    : " + errorThrown.message);
+    },
+    success: function (xml) {
+
+      var insertContents = '';
+      $('item', xml).each(function () {
+        var thisItems = $(this);
+        insertContents += '<li>';
+        insertContents += '<p class="shop-name">';
+        insertContents += thisItems.children('name').text();
+        insertContents += '</p class="shop-name">';
+        insertContents += '<p class="shop-address">';
+        insertContents += thisItems.children('address').text();
+        insertContents += '</p class="shop-address">';
+        insertContents += '</li>';
+      });
+      $('.linkbox-shoplist').append(insertContents);
+    }
+  });
+});
